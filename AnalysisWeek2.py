f0ff733e83703016ec03a1184510577e1c99ac29
Mark Kaganovich <mark@labmeeting.com>
2011-04-15 17:03:52 -0700

    all files

--- AnalysisWeek2.py
+++ AnalysisWeek2.py
@@ -38,23 +38,40 @@ syt = ['S', 'Y', 'T']
 aaset = syt
 binsize = 10
 
-def calcturnoverorfs(orfs, binsize, aapos):
+def calcturnoverorfs(orfs, binsize, aapos, aas):
     for k in orfs.keys():
         o = orfs[k]
-        aaset = getattr(o, aapos)
+        if hasattr(o, aapos):
+            aaset = getattr(o, aapos)
+        else:
+            continue
         if 'Scer' in o.kwalalign.keys():
-            o.turnover[binsize] = 0
-            for i,x in enumerate(o.kwalalign['Scer']):
-                if i in aaset and x != o.kwalalign['Kwal'][i]:
-                    slice = o.kwalalign['Kwal'].__getslice__(i-binsize/2,i+binsize/2)    
-                    inds = [l for l in range(0, len(slice)) if slice[l] == x]
-                    lost = filter(lambda y: o.kwalalign['Scer'][y] != x, inds)
-                    if lost != []:
-                        o.turnover[binsize] = len(lost) + o.turnover[binsize]
+            o.phosphoturnover[binsize] = 0
+            for i in aaset:
+                scer = o.kwalalign['Scer']
+                otherspecies = o.kwalalign['Kwal']
+                ai = o.kwalalignindex[i]
+                if scer[o.kwalalignindex[i]] != otherspecies[ai]:
+                    left = ai-binsize/2
+                    right = ai+binsize/2
+                    if left < 0:
+                        left = 0
+                    if right > len(o.kwalalignindex)-1:
+                        right = len(o.kwalalignindex)-1
+                    slice = otherspecies.__getslice__(left,right)    
+                    slicescer = scer.__getslice__(left,right)
+                    numsetinbin = (len(filter(lambda y: 
+                                    o.kwalalignindex[y] in aaset, range(left, right))))
+                    if numsetinbin > 0:
+                        inds = [l for l in range(0, len(slice)) if slice[l] == x]
+                        lost = (len(filter(lambda y: slicescer[y] != x, inds))
+                                /float(numsetinbin))
+                        o.phosphoturnover[binsize] = lost + o.phosphoturnover[binsize]
+            o.phosphoturnover[binsize] = o.phosphoturnover[binsize]/len(aaset)
 
-binsizes = range(0,300,10)
+binsizes = range(10,300,10)
 for b in binsizes:
-    calcturnoverorfs(orfs, b, aaset)
+    calcturnoverorfs(orfs, b, 'phosphosite', ['S', 'Y', 'T'])
 
 
 
